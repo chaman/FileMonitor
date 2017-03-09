9222dddf07228fce8bbc4895f7081043808cd0a9
chaman <chaman.thapa@gmail.com>
2017-03-08 23:02:29 -0500

    fix spaces

--- receiver.c
+++ receiver.c
@@ -3,13 +3,13 @@
 
 
 static void
-on_signal_received (GDBusConnection     *connection,
-                    const gchar         *sender_name,
-                    const gchar         *object_path,
-                    const gchar         *interface_name,
-                    const gchar         *signal_name,
-                    GVariant            *parameters,
-                    gpointer            *user_data)
+on_signal_received(GDBusConnection     *connection,
+                   const gchar         *sender_name,
+                   const gchar         *object_path,
+                   const gchar         *interface_name,
+                   const gchar         *signal_name,
+                   GVariant            *parameters,
+                   gpointer            *user_data)
 {
     gchar *msg;
     g_variant_get(parameters,"(s)",&msg);
@@ -19,18 +19,18 @@ on_signal_received (GDBusConnection     *connection,
                     
            
 static void
-on_bus_acquired (GDBusConnection *connection,
-                 const gchar     *name,
-                 gpointer         user_data)
+on_bus_acquired(GDBusConnection *connection,
+                const gchar     *name,
+                gpointer         user_data)
 {
 
 }
 
 
 static void
-on_name_acquired (GDBusConnection *connection,
-                  const gchar     *name,
-                  gpointer         user_data)
+on_name_acquired(GDBusConnection *connection,
+                 const gchar     *name,
+                 gpointer         user_data)
 {
     g_dbus_connection_signal_subscribe( connection,
                                         "org.filemon.Signal",
@@ -47,9 +47,9 @@ on_name_acquired (GDBusConnection *connection,
 }
 
 static void
-on_name_lost (GDBusConnection *connection,
-              const gchar     *name,
-              gpointer         user_data)
+on_name_lost(GDBusConnection *connection,
+             const gchar     *name,
+             gpointer         user_data)
 {
 
 }
@@ -65,20 +65,20 @@ main (int argc, char *argv[])
     g_type_init ();
     
     
-    owner_id = g_bus_own_name (G_BUS_TYPE_SESSION,
-                               "org.filemon.Slot",
-                               G_BUS_NAME_OWNER_FLAGS_REPLACE,
-                               on_bus_acquired,
-                               on_name_acquired,
-                               on_name_lost,
-                               NULL,
-                               NULL);
+    owner_id = g_bus_own_name(G_BUS_TYPE_SESSION,
+                              "org.filemon.Slot",
+                              G_BUS_NAME_OWNER_FLAGS_REPLACE,
+                              on_bus_acquired,
+                              on_name_acquired,
+                              on_name_lost,
+                              NULL,
+                              NULL);
     
                                        
 
-    loop = g_main_loop_new (NULL, FALSE);
-    g_main_loop_run (loop);
-    g_bus_unown_name (owner_id);
+    loop = g_main_loop_new(NULL, FALSE);
+    g_main_loop_run(loop);
+    g_bus_unown_name(owner_id);
 
     return 0;
 }
